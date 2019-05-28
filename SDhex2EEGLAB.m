80afa18bee6a749051ac5cb75818de6925a85966
Aleksandar Miladinović <alex.miladinovich@gmail.com>
2019-05-28 20:49:36 +0200

    Update SDhex2EEGLAB.m

--- SDhex2EEGLAB.m
+++ SDhex2EEGLAB.m
@@ -7,10 +7,15 @@ fileID=fopen(fullfile(path,file));
 disp('Please be patient. This will take some time...');
 wait=['.'];
 i=1;
+numOfDiscontinuity=0;
 while 1
     tline = fgetl(fileID);
     if ~ischar(tline), break, end
     splitLine=strsplit(tline,',');
+    if length(splitLine)==1 && splitLine{1}(1)=='%' %when there are %start and stop signs
+        numOfDiscontinuity=numOfDiscontinuity + 1;
+        discontinuity(numOfDiscontinuity)=i;
+    end
     if length(splitLine)>3
         arrayMicrovolts=hexToMicroVolts(splitLine);
         if length(arrayMicrovolts)==19
@@ -59,8 +64,16 @@ EEG=pop_chanedit(EEG, 'lookup','standard-10-5-cap385.elp');
 disp(['Saving file to: ' fullfile(path,[file(1:end-3) 'gdf'])]);
 pop_writeeeg(EEG, fullfile(path,[file(1:end-3) 'gdf']), 'TYPE','GDF');
 
+
+offset=length(EEG.event)+1
+for i=1:numOfDiscontinuity
+    EEG.event(i+offset).type='DIS';
+    EEG.event(i+offset).latency=discontinuity(i);
+    EEG.event(i+offset).duration=1;
+    EEG.event(i+offset).urevent=0;
+end
+
 disp('Opening EEGLAB...');
 eeglab redraw;
-
 
 
